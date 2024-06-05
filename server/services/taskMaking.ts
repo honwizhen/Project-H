91144ca5499d5b2516d4b67bcd68f873fda32476
Ernesto-B <ernesto.barreto@ucalgary.ca>
2024-06-04 19:32:54 -0600

    Finished taskMaking service and got rid of unneeded files

---
+++ server/services/taskMaking.ts
@@ -0,0 +1,27 @@
+// Date object
+const date = new Date();
+let currentDay= String(date.getDate()).padStart(2, '0');
+let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+let currentYear = date.getFullYear();
+// we will display the date as DD-MM-YYYY 
+let currentDate = `${currentDay}-${currentMonth}-${currentYear}`;
+// console.log("The current date is " + currentDate); 
+
+// Generate a unique taskId
+const generateHash = require('random-hash').default;
+const randomTaskId = generateHash({ length: 15 });
+
+function newTask(title:string, descr:string = "", shared:boolean = false): object {
+    const task = {
+        taskId: randomTaskId,
+        title: title,
+        descr: descr,
+        date_created: currentDate,
+        completed: false,
+        shared: shared
+    }
+    return task;
+}
+// console.log(newTask("Test Task", ""))
+
+module.exports = newTask;
---
+++ server/utils/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+/**
+ * Middleware to authenticate JWT token
+ * @name authenticateToken
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @param {function} next - Express next function
+ * @returns {void}
+ */
+function authenticateToken(req) {
+  const token = req.headers["authorization"];
+  // const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    throw new Error("No token provided");
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    return user;
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+}
+
+module.exports = authenticateToken;
