5739fa8c878474dd1f37bec5b2a519467550ca60
honwizhen <hzhen3720@gmail.com>
2024-09-24 05:26:32 -0600

    package for tailwind and posthog

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // missing path to all the template files
+  content: ["./app/**/*."],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
