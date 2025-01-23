669ee83ccad860e2bc95b0cc94858cdca2c95631
honwizhen <hzhen3720@gmail.com>
2025-01-22 18:53:27 -0700

    Tailwind update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,42 @@
-import { StyleSheet } from 'react-native';
+// import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+
+// export default function TabOneScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text className="text-3xl font-bold underline">Tab One</Text>
+//       <View  lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//       <EditScreenInfo path="app/(tabs)/index.tsx" />
+//     </View>
+//   );
+// }
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <h1 className="bg-slate-800 font-serif">Tabfe ef One</h1>
+    // <View className='bg-blue-500'>
+    //   <Text className="text-3xl font-bold underline bg-red-600">Tab Orih gvsrguinrsoine</Text>
+    //   <View  lightColor="#f" darkColor="rgba(255,255,255,0.1)" />
+    //   <EditScreenInfo path="app/(tabs)/index.tsx" />
+    // </View>
   );
 }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
